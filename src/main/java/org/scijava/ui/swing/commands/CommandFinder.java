1e49d607efa5e16b7ae0bb9c694858d9a3b0079a
Curtis Rueden <ctrueden@wisc.edu>
2014-04-21 21:55:24 -0500

    Migrate Swing code from ImageJ
    Code migrated from:
    * https://github.com/imagej/imagej/tree/imagej-2.0.0-beta-7.9/plugins/uis/swing
    * https://github.com/imagej/imagej/tree/imagej-2.0.0-beta-7.9/ui/swing

---
+++ src/main/java/org/scijava/ui/swing/commands/CommandFinder.java
@@ -0,0 +1,86 @@
+/*
+ * #%L
+ * SciJava UI components for Java Swing.
+ * %%
+ * Copyright (C) 2010 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ui.swing.commands;
+
+import javax.swing.JOptionPane;
+
+import org.scijava.app.AppService;
+import org.scijava.command.Command;
+import org.scijava.command.ContextCommand;
+import org.scijava.menu.MenuConstants;
+import org.scijava.module.ModuleInfo;
+import org.scijava.module.ModuleService;
+import org.scijava.plugin.Menu;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.swing.SwingDialog;
+
+/**
+ * A plugin to display the {@link CommandFinderPanel} in a dialog.
+ * 
+ * @author Curtis Rueden
+ * @author Johannes Schindelin
+ */
+@Plugin(type = Command.class, menu = {
+	@Menu(label = MenuConstants.PLUGINS_LABEL,
+		weight = MenuConstants.PLUGINS_WEIGHT,
+		mnemonic = MenuConstants.PLUGINS_MNEMONIC), @Menu(label = "Utilities"),
+	@Menu(label = "Find Commands...", accelerator = "^L") })
+public class CommandFinder extends ContextCommand {
+
+	@Parameter
+	private ModuleService moduleService;
+
+	@Parameter
+	private AppService appService;
+
+	@Override
+	public void run() {
+		final String baseDir =
+			appService.getApp().getBaseDirectory().getAbsolutePath();
+		final CommandFinderPanel commandFinderPanel =
+			new CommandFinderPanel(moduleService, baseDir);
+		final SwingDialog dialog =
+			new SwingDialog(commandFinderPanel, JOptionPane.OK_CANCEL_OPTION,
+				JOptionPane.PLAIN_MESSAGE, false);
+		dialog.setFocus(commandFinderPanel.getSearchField());
+		dialog.setTitle("Find Commands");
+		final int rval = dialog.show();
+		if (rval != JOptionPane.OK_OPTION) return; // dialog canceled
+
+		final ModuleInfo info = commandFinderPanel.getCommand();
+		if (info == null) return; // no command selected
+
+		// execute selected command
+		moduleService.run(info, true);
+	}
+
+}
---
+++ src/main/java/org/scijava/ui/swing/sdi/SwingUI.java
@@ -0,0 +1,101 @@
+/*
+ * #%L
+ * SciJava UI components for Java Swing.
+ * %%
+ * Copyright (C) 2010 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ui.swing.sdi;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+import org.scijava.display.Display;
+import org.scijava.event.EventService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.DialogPrompt;
+import org.scijava.ui.DialogPrompt.MessageType;
+import org.scijava.ui.DialogPrompt.OptionType;
+import org.scijava.ui.UserInterface;
+import org.scijava.ui.awt.AWTDropTargetEventDispatcher;
+import org.scijava.ui.awt.AWTInputEventDispatcher;
+import org.scijava.ui.awt.AWTWindowEventDispatcher;
+import org.scijava.ui.swing.AbstractSwingUI;
+import org.scijava.ui.swing.SwingApplicationFrame;
+import org.scijava.ui.swing.viewer.SwingDisplayWindow;
+
+/**
+ * Swing-based SDI user interface for ImageJ.
+ * 
+ * @author Curtis Rueden
+ * @author Grant Harris
+ */
+@Plugin(type = UserInterface.class, name = SwingUI.NAME)
+public class SwingUI extends AbstractSwingUI {
+
+	public static final String NAME = "swing";
+
+	@Parameter
+	private EventService eventService;
+
+	// -- UserInterface methods --
+
+	@Override
+	public SwingDisplayWindow createDisplayWindow(final Display<?> display) {
+		final SwingDisplayWindow displayWindow = new SwingDisplayWindow();
+
+		// broadcast input events (keyboard and mouse)
+		new AWTInputEventDispatcher(display).register(displayWindow, true, false);
+
+		// broadcast window events
+		new AWTWindowEventDispatcher(display).register(displayWindow);
+
+		// broadcast drag-and-drop events
+		new AWTDropTargetEventDispatcher(display, eventService);
+
+		return displayWindow;
+	}
+
+	@Override
+	public DialogPrompt dialogPrompt(final String message, final String title,
+		final MessageType msg, final OptionType option)
+	{
+		return new SwingDialogPrompt(message, title, msg, option);
+	}
+
+	// -- Internal methods --
+
+	@Override
+	protected void setupAppFrame() {
+		final SwingApplicationFrame appFrame = getApplicationFrame();
+		final JPanel pane = new JPanel();
+		appFrame.setContentPane(pane);
+		pane.setLayout(new BorderLayout());
+	}
+
+}
