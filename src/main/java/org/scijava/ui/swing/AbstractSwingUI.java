3981aff9603c66544993031b61f5cd395c44505e
Curtis Rueden <ctrueden@wisc.edu>
2015-07-27 19:00:44 -0400

    Add a Clear command for the console
    Closes #5.

--- src/main/java/org/scijava/ui/swing/AbstractSwingUI.java
+++ src/main/java/org/scijava/ui/swing/AbstractSwingUI.java
@@ -32,12 +32,16 @@ package org.scijava.ui.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 
 import javax.swing.JFileChooser;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.WindowConstants;
 
@@ -235,6 +239,22 @@ public abstract class AbstractSwingUI extends AbstractUserInterface implements
 			return null;
 		}
 		appMenusCreatedEvent.consume();
+		return menuBar;
+	}
+
+	protected JMenuBar createConsoleMenu() {
+		final JMenuBar menuBar = new JMenuBar();
+		final JMenu edit = new JMenu("Edit");
+		menuBar.add(edit);
+		final JMenuItem editClear = new JMenuItem("Clear");
+		editClear.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getConsolePane().clear();
+			}
+		});
+		edit.add(editClear);
 		return menuBar;
 	}
 
