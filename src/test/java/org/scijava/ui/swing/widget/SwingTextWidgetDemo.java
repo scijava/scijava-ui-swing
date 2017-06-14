f959e97593cffe7c0e4e31ee928a93219ec34409
Matthias Arzt <arzt@mpi-cbg.de>
2017-06-14 15:27:36 +0200

    SwingTextWidget: fix preferred size calculation of TextWidget.AREA_STYLE
    The JTextArea must be put into a JScrollPane, otherwise the LayoutManger
    causes a NullPointerException when calculating the preferred size.

---
+++ src/test/java/org/scijava/ui/swing/widget/SwingTextWidgetDemo.java
@@ -0,0 +1,73 @@
+/*
+ * #%L
+ * SciJava UI components for Java Swing.
+ * %%
+ * Copyright (C) 2010 - 2017 Board of Regents of the University of
+ * Wisconsin-Madison.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package org.scijava.ui.swing.widget;
+
+/**
+ * Demonstrate SwingTextWidget
+ *
+ * @author Matthias Arzt
+ */
+
+import org.scijava.Context;
+import org.scijava.command.Command;
+import org.scijava.command.CommandService;
+import org.scijava.plugin.Parameter;
+import org.scijava.ui.UIService;
+import org.scijava.widget.TextWidget;
+
+public class SwingTextWidgetDemo implements Command {
+
+	@Parameter(label = "Default")
+	private String string;
+
+	@Parameter(label = "Password Style", style = TextWidget.PASSWORD_STYLE)
+	private String password;
+
+	@Parameter(label = "Field Style", style = TextWidget.FIELD_STYLE)
+	private String field;
+
+	@Parameter(label = "Area Style", style = TextWidget.AREA_STYLE)
+	private String area;
+
+	@Override
+	public void run() {
+		System.out.println(string);
+		System.out.println(password);
+		System.out.println(field);
+		System.out.println(area);
+	}
+
+	public static void main(final String... args) throws Exception {
+		Context context = new Context();
+		context.service(UIService.class).showUI();
+		context.service(CommandService.class).run(SwingTextWidgetDemo.class, true);
+	}
+}
