af2e2d9fabb54a3941f57161f1657b355f6d1df9
Curtis Rueden <ctrueden@wisc.edu>
2018-04-27 14:24:26 -0700

    LogFormatterTest: improve the settings test
    We make it reproducible by clearing previously persisted values first.
    Then check that indeed, the persistence applies (only) where it should.

--- src/test/java/org/scijava/ui/swing/console/LogFormatterTest.java
+++ src/test/java/org/scijava/ui/swing/console/LogFormatterTest.java
@@ -17,11 +17,24 @@ public class LogFormatterTest {
 	@Test
 	public void testSettings() {
 		Context context = new Context(PrefService.class);
-		LogFormatter formatter1 = new LogFormatter(context, "abc");
+		final String usedKey = "LogFormatterTest-key";
+		final String nonMatchingKey = "LogFormatterTest-nonmatching";
+		context.service(PrefService.class).remove(LogFormatter.class, usedKey);
+		context.service(PrefService.class).remove(LogFormatter.class, nonMatchingKey);
+
+		// Assign some settings to a particular key.
+		LogFormatter formatter1 = new LogFormatter(context, usedKey);
 		formatter1.setVisible(LogFormatter.Field.ATTACHMENT, true);
 		formatter1.setVisible(LogFormatter.Field.LEVEL, false);
-		LogFormatter formatter2 = new LogFormatter(context, "abc");
+
+		// Check that matching key does share these settings.
+		LogFormatter formatter2 = new LogFormatter(context, usedKey);
 		assertTrue(formatter2.isVisible(LogFormatter.Field.ATTACHMENT));
 		assertFalse(formatter2.isVisible(LogFormatter.Field.LEVEL));
+
+		// Check that non-matching key does not share these settings.
+		LogFormatter formatter3 = new LogFormatter(context, nonMatchingKey);
+		assertFalse(formatter3.isVisible(LogFormatter.Field.ATTACHMENT));
+		assertTrue(formatter3.isVisible(LogFormatter.Field.LEVEL));
 	}
 }
