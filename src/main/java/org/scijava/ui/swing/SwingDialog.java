7da28cdc99a4d0a9011d91979c382274cc083aba
Curtis Rueden <ctrueden@wisc.edu>
2020-02-15 19:12:48 -0600

    Happy New Year 2020

--- src/main/java/org/scijava/ui/swing/SwingDialog.java
+++ src/main/java/org/scijava/ui/swing/SwingDialog.java
@@ -2,8 +2,7 @@
  * #%L
  * SciJava UI components for Java Swing.
  * %%
- * Copyright (C) 2010 - 2017 Board of Regents of the University of
- * Wisconsin-Madison.
+ * Copyright (C) 2010 - 2020 SciJava developers.
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
