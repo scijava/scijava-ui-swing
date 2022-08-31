87fe1e4a1c02ff3b9f98a32192a8c39d16284a85
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-08-31 21:50:26 +0200

    Fixes interactive command NPEs raised in SwingObjectWidget
    Any widget model which has no choice available will be not be handled by a SwingObjectWidget.
    
    Adds an interactive command test.

--- src/test/java/org/scijava/ui/swing/widget/SwingObjectWidgetTest.java
+++ src/test/java/org/scijava/ui/swing/widget/SwingObjectWidgetTest.java
@@ -39,6 +39,7 @@ import org.junit.Test;
 import org.scijava.Context;
 import org.scijava.command.CommandInfo;
 import org.scijava.command.ContextCommand;
+import org.scijava.command.InteractiveCommand;
 import org.scijava.module.Module;
 import org.scijava.module.ModuleItem;
 import org.scijava.module.ModuleService;
@@ -67,11 +68,38 @@ public class SwingObjectWidgetTest {
 	}
 
 	@Test
-	public void testObjects() {
+	public void testObjectsCommand() {
 		Thing a = new Thing();
 		Thing b = new Thing();
 
 		CommandInfo commandInfo = new CommandInfo(MyCommand.class);
+		Module module = moduleService.createModule(commandInfo);
+		InputPanel<?,?> panel = new SwingInputPanel();
+
+		ModuleItem<Thing> thingInput = moduleService.getSingleInput(module, Thing.class);
+		ModuleItem<Nothing> nothingInput = moduleService.getSingleInput(module, Nothing.class);
+		ModuleItem<Choices> choicesInput = moduleService.getSingleInput(module, Choices.class);
+
+		WidgetModel thingModel = widgetService.createModel(panel, module, thingInput, Arrays.asList(a, b));
+		WidgetModel nothingModel = widgetService.createModel(panel, module, nothingInput, null);
+		WidgetModel choicesModel = widgetService.createModel(panel, module, choicesInput, null);
+
+		InputWidget<?, ?> thingWidget = widgetService.create(thingModel);
+		assertTrue(thingWidget instanceof SwingObjectWidget);
+
+		InputWidget<?, ?> nothingWidget = widgetService.create(nothingModel);
+		assertFalse(nothingWidget instanceof SwingObjectWidget);
+
+		InputWidget<?, ?> choicesWidget = widgetService.create(choicesModel);
+		assertTrue(choicesWidget instanceof SwingObjectWidget);
+	}
+
+	@Test
+	public void testObjectsInteractiveCommand() {
+		Thing a = new Thing();
+		Thing b = new Thing();
+
+		CommandInfo commandInfo = new CommandInfo(MyInteractiveCommand.class);
 		Module module = moduleService.createModule(commandInfo);
 		InputPanel<?,?> panel = new SwingInputPanel();
 
@@ -121,4 +149,22 @@ public class SwingObjectWidgetTest {
 		}
 
 	}
+
+	public static class MyInteractiveCommand extends InteractiveCommand {
+		@Parameter
+		private Thing thing;
+
+		@Parameter
+		private Nothing nothing;
+
+		@Parameter
+		private Choices choices;
+
+		@Override
+		public void run() {
+			// nothing to do
+		}
+
+	}
+
 }
